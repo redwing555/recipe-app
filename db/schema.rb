d8cc99117b5b897ab0f9e3c89ef7b833df51fc98
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-31 16:56:04 +0100

    create a recipes controller + recipes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_153443) do
+ActiveRecord::Schema.define(version: 2022_01_31_154751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "prepartion_time"
+    t.decimal "cooking_time"
+    t.text "description"
+    t.boolean "public", default: true
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_01_31_153443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
