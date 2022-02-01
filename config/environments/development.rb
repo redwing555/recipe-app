a45460bcd5744c920ca954ecb5beb4e560b7f280
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-01 15:29:08 +0100

    add confirmation email set up after sign_in

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
