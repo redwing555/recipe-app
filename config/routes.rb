886b0eefa73c1e2a85eb8fc057fb50ac8a626d22
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-02 02:15:18 +0100

    work on email confirmation+styling login and signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
   devise_for :users
+
+  
   resources :food, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :create, :destroy]
 
