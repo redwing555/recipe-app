188896054d13d9209053e3d62f65fe62f16ac14c
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-02 18:02:26 +0100

    fix rubocop linters

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,41 +1,39 @@
 class RecipeFoodController < ApplicationController
+  before_action :current_user_foods, only: %i[new create]
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new
+  end
 
-    before_action :current_user_foods, only: %i[new create] 
-    def new
-        @recipe = Recipe.find(params[:recipe_id])
-        @recipe_food = @recipe.recipe_foods.new
+  def current_user_foods
+    @foods = current_user.foods.collect { |food| [food.name, food.id] }
+  end
+
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    if @recipe_food.save
+      flash[:notice] = "Food added to Recipe successfully!"
+      redirect_to recipe_path(@recipe_food.recipe_id)
+    else
+      flash[:alert] = 'Food not added ! please try again'
+      render :new
+    end
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.destroy
+      flash[:notice] = 'Food removed successfully'
+    else
+      flash[:alert] = 'Food not removed ! please try again'
     end
 
-    def current_user_foods
-        @foods = current_user.foods.collect { |food| [food.name, food.id] }
-      end
-    
+    redirect_to recipe_path(@recipe_food.recipe_id)
+  end
 
-    def create
-        @recipe_food= RecipeFood.new(recipe_food_params)
-        if @recipe_food.save
-            flash[:notice] = "Food added to Recipe successfully!"
-            redirect_to recipe_path(@recipe_food.recipe_id)
-        else
-            flash[:alert] = 'Food not added ! please try again'
-            render :new
-        end
-    end
+  private
 
-    def destroy
-        @recipe_food = RecipeFood.find(params[:id])
-        if @recipe_food.destroy
-            flash[:notice] = 'Food removed successfully'
-        else
-            flash[:alert] = 'Food not removed ! please try again'   
-        end
-
-        redirect_to recipe_path(@recipe_food.recipe_id)
-    end
-
-    private
-
-    def recipe_food_params
-        params.require(:recipe_food).permit(:food_id,:recipe_id, :quantity)
-    end
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :recipe_id, :quantity)
+  end
 end
