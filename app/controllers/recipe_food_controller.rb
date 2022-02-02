46c8655f2763a96c48e066aebe455008e3e2e552
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-02 15:03:15 +0100

    recipe food controller actions

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,3 +1,34 @@
 class RecipeFoodController < ApplicationController
-    
+    def new
+        @recipe = Recipe.find(params[:recipe_id])
+        @recipe_food = @recipe.recipe_foods.new
+    end
+
+    def create
+        @recipe_food= RecipeFood.new(recipe_food_params)
+        if @recipe_food.save
+            flash[:notice] = "Food added to Recipe successfully!"
+            redirect_to recipe_path(@recipe_food.recipe_id)
+        else
+            flash[:alert] = 'Food not added ! please try again'
+            render :new
+        end
+    end
+
+    def destroy
+        @recipe_food = RecipeFood.find(params[:id])
+        if @recipe_food.destroy
+            flash[:notice] = 'Food removed successfully'
+        else
+            flash[:alert] = 'Food not removed ! please try again'   
+        end
+
+        redirect_to recipe_path(@recipe_food.recipe_id)
+    end
+
+    private
+
+    def recipe_food_params
+        params.require(:recipe_food).permit(:food_id,:recipe_id, :quantity)
+    end
 end
