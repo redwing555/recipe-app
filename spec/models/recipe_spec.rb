017c8b2d20daf07fb1a57d4b1b6dd73abb70febe
Hichami Houssam <hichami.houssam@gmail.com>
2022-02-03 18:30:41 +0100

    fix linters

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,30 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-    describe 'Fields validations' do
-        subject do
-            @user1 = User.new( name: "testUser", email: "test@gmail.com")
-            Recipe.create!(name: 'Recipe', preparation_time: 1,  cooking_time: 2, description: 'lorem upsum',user: @user1, user_id: @user1.id)
-           
-          end
-        it 'name should be present' do
-          subject.name = nil
-          expect(subject).to_not be_valid
-        end
-    
-        it 'preparation time should be present' do
-          subject.preparation_time = nil
-          expect(subject).to_not be_valid
-        end
-    
-        it 'cooking time should be present' do
-          subject.cooking_time = nil
-          expect(subject).to_not be_valid
-        end
-    
-        it 'description should be present' do
-          subject.description = nil
-          expect(subject).to_not be_valid
-        end
-      end
+  describe 'Fields validations' do
+    subject do
+      @user1 = User.new(name: "testUser", email: "test@gmail.com")
+      Recipe.create!(name: 'Recipe', preparation_time: 1, cooking_time: 2, description: 'lorem upsum', user: @user1, user_id: @user1.id)
+    end
+    it 'name should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'preparation time should be present' do
+      subject.preparation_time = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'cooking time should be present' do
+      subject.cooking_time = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'description should be present' do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-    
-    describe 'validations' do
-        subject do
-            User.create!( name: "testUser", email: "test@gmail.com", password: '123455')
-          end
+  describe 'validations' do
+    subject do
+      User.create!(name: "testUser", email: "test@gmail.com", password: '123455')
+    end
     it 'name should be present' do
       expect(subject.name).to eq 'testUser'
     end
